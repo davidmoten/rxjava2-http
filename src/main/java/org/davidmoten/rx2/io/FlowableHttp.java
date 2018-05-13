09d3fad5c333c42f6fe90c277706f83cdedb9ab5
Dave Moten <davidmoten@gmail.com>
2018-05-13 21:26:22 +1000

    dev

--- src/main/java/org/davidmoten/rx2/io/FlowableHttp.java
+++ src/main/java/org/davidmoten/rx2/io/FlowableHttp.java
@@ -1,5 +1,6 @@
 package org.davidmoten.rx2.io;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,8 +15,11 @@ import org.reactivestreams.Subscription;
 
 import io.reactivex.Flowable;
 import io.reactivex.exceptions.Exceptions;
+import io.reactivex.internal.fuseable.SimplePlainQueue;
+import io.reactivex.internal.queue.SpscLinkedArrayQueue;
 import io.reactivex.internal.subscriptions.SubscriptionHelper;
 import io.reactivex.internal.util.BackpressureHelper;
+import io.reactivex.plugins.RxJavaPlugins;
 
 public class FlowableHttp extends Flowable<ByteBuffer> {
 
@@ -43,6 +47,7 @@ public class FlowableHttp extends Flowable<ByteBuffer> {
         private final Subscriber<? super ByteBuffer> child;
         private URLConnection con;
         private final AtomicLong requested = new AtomicLong();
+        private final SimplePlainQueue<ByteBuffer> queue = new SpscLinkedArrayQueue<>(16);
 
         private boolean firstTime = true;
 
@@ -52,9 +57,16 @@ public class FlowableHttp extends Flowable<ByteBuffer> {
 
         private Throwable error;
         private volatile boolean finished;
+        private long emitted;
 
-        HttpSubscription(URL url, int bufferSize, Subscriber<? super ByteBuffer> child) {
+        private final int preRequest;
+
+        private volatile boolean cancelled;
+
+        HttpSubscription(URL url, int preRequest, int bufferSize,
+                Subscriber<? super ByteBuffer> child) {
             this.url = url;
+            this.preRequest = preRequest;
             this.bufferSize = bufferSize;
             this.child = child;
             lazySet(1);
@@ -65,6 +77,7 @@ public class FlowableHttp extends Flowable<ByteBuffer> {
                 con = url.openConnection();
                 out = con.getOutputStream();
                 in = con.getInputStream();
+                requestViaOutputStream(preRequest);
                 set(0);
                 drain();
             } catch (Throwable e) {
@@ -78,25 +91,108 @@ public class FlowableHttp extends Flowable<ByteBuffer> {
         public void request(long n) {
             if (SubscriptionHelper.validate(n)) {
                 BackpressureHelper.add(requested, n);
-                try {
-                    out.write(Util.toBytes(n));
-                } catch (Throwable e) {
-                    error = e;
-                    finished = true;
-                }
+                requestViaOutputStream(n);
                 drain();
+            }
+        }
+
+        private void requestViaOutputStream(long n) {
+            try {
+                out.write(Util.toBytes(n));
+            } catch (Throwable e) {
+                error = e;
+                finished = true;
             }
         }
 
         private void drain() {
             if (getAndIncrement() == 0) {
+                int missed = 1;
+                while (true) {
+                    if (tryCancelled()) {
+                        return;
+                    }
+                    long r = requested.get();
+                    long e = emitted;
+                    while (e != r) {
+                        boolean d = finished;
+                        ByteBuffer bb = queue.poll();
+                        if (bb == null) {
+                            if (d) {
+                                Throwable err = error;
+                                if (err != null) {
+                                    error = null;
+                                    child.onError(err);
+                                } else {
+                                    child.onComplete();
+                                }
+                            } else {
+                                // read some more
+                                byte[] b = new byte[bufferSize];
+                                try {
+                                    int n = in.read(b);
+                                    if (n == -1) {
+                                        close(in);
+                                        close(out);
+                                        finished = true;
+                                    } else {
+                                        queue.offer(bb);
+                                    }
+                                } catch (Throwable e1) {
+                                    Exceptions.throwIfFatal(e1);
+                                    close(in);
+                                    close(out);
+                                    error = e1;
+                                    finished = true;
+                                }
+                            }
+                        } else {
+                            child.onNext(bb);
+                            e++;
+                            if (preRequest > 0 && e % preRequest == 0) {
+                                // prefetch
+                                requestViaOutputStream(preRequest);
+                            }
+                        }
+                        if (tryCancelled()) {
+                            return;
+                        }
+                    }
+                    emitted = e;
+                    missed = addAndGet(-missed);
+                    if (missed == 0) {
+                        return;
+                    }
 
+                }
             }
+        }
+
+        private boolean tryCancelled() {
+            if (cancelled) {
+                close(in);
+                close(out);
+                queue.clear();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void close(Closeable c) {
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (IOException e) {
+                    RxJavaPlugins.onError(e);
+                }
+            }
+
         }
 
         @Override
         public void cancel() {
-            // TODO Auto-generated method stub
+            cancelled = true;
 
         }
 
