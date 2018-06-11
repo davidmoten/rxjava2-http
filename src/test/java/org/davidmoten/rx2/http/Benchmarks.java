164759eca83966529c7f480691e87f4fe3def0b3
Dave Moten <davidmoten@gmail.com>
2018-06-12 07:04:13 +1000

    parameterize benchmarks

--- src/test/java/org/davidmoten/rx2/http/Benchmarks.java
+++ src/test/java/org/davidmoten/rx2/http/Benchmarks.java
@@ -4,11 +4,11 @@ import java.nio.ByteBuffer;
 
 import org.davidmoten.rx2.io.Client;
 import org.davidmoten.rx2.io.Servers;
-import org.davidmoten.rx2.io.internal.Util;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -19,18 +19,21 @@ import io.reactivex.Flowable;
 @State(Scope.Benchmark)
 public class Benchmarks {
 
-    private static final int TOTAL_BYTES = 8 * 1024 * 1024;
-    private static final int BYTES_PER_ITEM = 8192 * 8;
+    private static final int TOTAL_BYTES = 64 * 1024 * 1024;
+    private static final int BYTES_PER_ITEM = 1024;
 
     @State(Scope.Thread)
     public static class ServerHolder {
 
-        Server server = null;
+        public Server server = null;
+
+        @Param({ "2", "8", "32", "128", "512", "2048", "8192", "32768", "65536", "131072" })
+        int bytesPerItem;
 
         @Setup
         public void setup() throws Exception {
             server = Servers.createServerAsync(
-                    Flowable.just(ByteBuffer.wrap(new byte[8192])).repeat());
+                    Flowable.just(ByteBuffer.wrap(new byte[bytesPerItem])).repeat());
             server.start();
         }
 
@@ -48,7 +51,7 @@ public class Benchmarks {
         return Client //
                 .get("http://localhost:" + port(holder.server)) //
                 .build() //
-                .rebatchRequests(16) //
+                .rebatchRequests(Math.max(TOTAL_BYTES / BYTES_PER_ITEM / 1024, 1)) //
                 .take(TOTAL_BYTES / BYTES_PER_ITEM) //
                 .count() //
                 .blockingGet();
